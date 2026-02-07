f9f4e201c29f407861bbc55db0e12c134e93da93
Anh Nhật <nhathuynh04work@gmail.com>
2026-02-07 17:06:49 +0700

    feat: add validation

--- api/src/auth/auth.dto.ts
+++ api/src/auth/auth.dto.ts
@@ -1,5 +1,12 @@
-export interface LoginDto {
+import { IsEmail, IsString } from 'class-validator';
+import { ToLowerCase } from 'src/common/decorators/transform.decorators';
+
+export class LoginDto {
+  @IsEmail()
+  @ToLowerCase()
   email: string;
+
+  @IsString()
   password: string;
 }
 
