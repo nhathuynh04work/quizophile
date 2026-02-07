6e54d7fa04e57af7255f7e7eaa7cb8516130484e
Anh Nhật <nhathuynh04work@gmail.com>
2026-02-07 14:54:10 +0700

    init user module

---
+++ api/src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'src/generated/prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+
+    super({ adapter });
+  }
+  async onModuleDestroy() {
+    await this.$connect();
+  }
+
+  async onModuleInit() {
+    await this.$disconnect();
+  }
+}
