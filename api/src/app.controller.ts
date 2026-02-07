5653486a771fb0b1450a22efe999fedeafb9801b
Anh Nhật <nhathuynh04work@gmail.com>
2026-02-07 17:36:13 +0700

    feat: currentUser decorator

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/jwt.guard';
+import { CurrentUser } from './common/decorators/user.decorator';
+import { type JwtPayload } from './auth/auth.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  // @UseGuards(JwtAuthGuard)
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(JwtAuthGuard)
+  getHello(@CurrentUser() currentUser: JwtPayload) {
+    return currentUser;
   }
 }
--- api/src/auth/jwt.strategy.ts
+++ api/src/auth/jwt.strategy.ts
@@ -15,6 +15,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   validate(payload: JwtPayload) {
-    return { id: payload.sub, email: payload.email };
+    return payload;
   }
 }
