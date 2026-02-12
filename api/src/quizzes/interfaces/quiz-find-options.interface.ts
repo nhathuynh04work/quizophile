c3fd77cfe5f40639576c128a5bd15555b6622bd4
Anh Nhật <nhathuynh04work@gmail.com>
2026-02-12 23:58:32 +0700

    implement finding quiz with options

---
+++ api/src/quizzes/interfaces/quiz-find-options.interface.ts
@@ -0,0 +1,5 @@
+export interface FindQuizOptions {
+  withQuestions?: boolean;
+  withOptions?: boolean;
+  withAnswers?: boolean;
+}
--- api/src/quizzes/quizzes.service.ts
+++ api/src/quizzes/quizzes.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { FindQuizOptions } from './interfaces/quiz-find-options.interface';
 
 @Injectable()
-export class QuizzesService {}
+export class QuizzesService {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string, options: FindQuizOptions = {}) {
+    const {
+      withQuestions = false,
+      withOptions = false,
+      withAnswers = false,
+    } = options;
+
+    const quiz = await this.prisma.quiz.findUnique({
+      where: { id },
+      include: {
+        questions: withQuestions
+          ? {
+              orderBy: { order: 'asc' },
+              include: {
+                options: withOptions
+                  ? {
+                      omit: {
+                        isCorrect: !withAnswers,
+                      },
+                    }
+                  : false,
+              },
+            }
+          : false,
+      },
+    });
+
+    if (!quiz) {
+      throw new NotFoundException(`Quiz with ID ${id} not found`);
+    }
+
+    return quiz;
+  }
+}
